3f6023588eadb3ec6ac7fa4d537c49278332ebf2
000Linna <147407037+000Linna@users.noreply.github.com>
2024-09-28 14:25:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let res
+res = 10 + 20;
+res = 10 - 20;
+res = 10 * 20;
+res = 10 / 20;
+// остаток от деления %
+console.log(10 % 4)
+console.log(15 % 8)
+// возведение в степень*
+
+console.log(2 ** 2);
+console.log(2 ** 3);
+
+// возведение в корень
+console.log(50 ** (1 / 2));
+
+// бинарный плюс
+let str = " моя " + "строка"
+console.log(str)
+
+let a = 1;
+let b = "2";
+console.log(a + b)
+
+console.log(a / b)
+
+// унарный плюс - приводит к числу 
+console.log(a + + b)
+
+
+let apples = "2";
+let orange = "3";
+let sam = +apples + +orange;
+console.log(sam);
+
+// coкращенная арифметика +,-,/,*,...
+let n = 2
+n = n + 10
+console.log(n)
+
+// инкримент дикримент
+console.log("инкримент дикримент")
+let count = 10;
+count = count + 1;
+
+let result = count++ /* равноценно count = count + 1;*/
+console.log(count++);
+console.log(result + 5);
